ab3e443ad7c9e087d501ac9baa21f7df914c034b
Chloé <rightfold+ck@gmail.com>
2019-07-14 17:23:17 +0200

    Use rakudo-nix

--- nix/default.nix
+++ nix/default.nix
@@ -3,14 +3,14 @@ let
         url = "https://github.com/NixOS/nixpkgs/archive/8c09536ef10e72c9421c05403c68134480ba7fde.tar.gz";
         sha256 = "1kf8dl583lbabw93lpc4m5f62pqsip870d8n8hgw06s8898l7mkp";
     };
-    pkgsConfig = {
-        packageOverrides = pkgs: {
-            moar = pkgs.callPackage ./moar.nix {};
-            nqp = pkgs.callPackage ./nqp.nix {};
-            rakudo = pkgs.callPackage ./rakudo.nix {};
-            sixty = pkgs.callPackage ./sixty.nix {};
+    rakudo-nix = fetchTarball {
+        url = "https://github.com/chloekek/rakudo-nix/archive/2019.03.tar.gz";
+        sha256 = "0f48rvl4sy1p464fkkxmzz9hwgzym3yg1hfzmm2bqx2qg063ipkh";
+    };
+    pkgs = import pkgsSrc {
+        config = {
+            packageOverrides = import rakudo-nix;
         };
     };
-    pkgs = import pkgsSrc {config = pkgsConfig;};
 in
-    pkgs.sixty
+    pkgs.callPackage ./sixty.nix {}
